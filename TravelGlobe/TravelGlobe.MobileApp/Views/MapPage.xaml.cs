b13721f474e0077127acc278deeb7337a28cf335
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-07-25 16:05:55 +0200

    dodano mape z krajami

--- TravelGlobe/TravelGlobe.MobileApp/Views/MapPage.xaml.cs
+++ TravelGlobe/TravelGlobe.MobileApp/Views/MapPage.xaml.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System.Text.Json;
+using Microsoft.Maui.Controls;
 using TravelGlobe.MobileApp.ViewModels;
 
-namespace TravelGlobe.MobileApp.Views
+namespace TravelGlobe.MobileApp.Views;
+
+public partial class MapPage : ContentPage
 {
-    public partial class MapPage : ContentPage
+    public MapPage(MapViewModel viewModel)
     {
-        public MapPage(MapViewModel viewModel)
+        InitializeComponent();
+        BindingContext = viewModel;
+        MapWebView.Navigated += OnMapNavigated;
+    }
+
+    private async void OnMapNavigated(object sender, WebNavigatedEventArgs e)
+    {
+        if (BindingContext is MapViewModel vm)
         {
-            InitializeComponent();
-            BindingContext = viewModel;
+            var json = vm.GetMapDataJson();
+            var script = $"(function(){{var d={json};updateVisitedCountries(d.countries);updateAirports(d.airports);}})();";
+            await MapWebView.EvaluateJavaScriptAsync(script);
         }
     }
 }
