a814a97a6919f59ed32d8d7b3d439c59d0bcafde
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-07-25 15:25:45 +0200

    Naprawiono błąd rejestracji serwisów

--- TravelGlobe/TravelGlobe.Application/Extensions/ApplicationModule.cs
+++ TravelGlobe/TravelGlobe.Application/Extensions/ApplicationModule.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using TravelGlobe.Application.Services;
 using TravelGlobe.Infrastructure;
+using TravelGlobe.Domain.Factories;
+using TravelGlobe.Domain.ValueObjects;
 
 namespace TravelGlobe.Application;
 
@@ -10,6 +12,8 @@ public static class ApplicationModule
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration config)
     {
         services.AddInfrastructure(config);
+        services.AddScoped<IDistanceCalculator, DistanceCalculator>();
+        services.AddScoped<ITripFactory, TripFactory>();
         services.AddScoped<ITripService, TripService>();
 
         return services;
