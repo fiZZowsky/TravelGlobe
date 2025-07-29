408faf27622b77af0b1f710574b52543aea6d171
Piotr Równicki <piotr.rownicki@gearcode.eu>
2025-07-29 13:31:17 +0200

    Dodano wyświetlanie na profilu ustawionego przez użytkownika pseudonimu i kraju pochodzenia

--- TravelGlobe/TravelGlobe.MobileApp/ViewModels/ProfileViewModel.cs
+++ TravelGlobe/TravelGlobe.MobileApp/ViewModels/ProfileViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Maui.Controls;
 using System.Threading.Tasks;
+using Microsoft.Maui.Storage;
 using System.Windows.Input;
 using TravelGlobe.Application.Services;
 
@@ -8,6 +9,8 @@ namespace TravelGlobe.MobileApp.ViewModels
     public class ProfileViewModel : BindableObject
     {
         private readonly ITripService _tripService;
+        public string Nickname { get; }
+        public string Country { get; }
         public int VisitedCount { get; private set; }
         public double VisitedPercentage { get; private set; }
         public int TotalDistanceKm { get; private set; }
@@ -16,6 +19,8 @@ namespace TravelGlobe.MobileApp.ViewModels
         public ProfileViewModel(ITripService tripService)
         {
             _tripService = tripService;
+            Nickname = Preferences.Get("UserNickname", string.Empty);
+            Country = Preferences.Get("UserCountry", string.Empty);
             RefreshCommand = new Command(async () => await LoadStats());
             _ = LoadStats();
         }
